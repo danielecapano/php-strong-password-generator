09cd20b53bc8064a25cad1ad18c817b46817ec93
danielecapano <daniele.capano@gmail.com>
2023-11-25 02:11:08 +0100

    FIX - function getRandomPassword

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function getRandomPassword($length, $chars){
     $password = '';
     for($i = 0; $i <= $length -1; $i++){
-        $index = rand(0, strlen($chars));
+        $index = rand(0, strlen($chars) - 1);
         $char = substr($chars, $index, 1);
         $password .= $char;
     }
